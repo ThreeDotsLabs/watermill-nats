0745542f1c7ab6cee0410d4aa703d21ef1fa4339
Alex <sysradium@users.noreply.github.com>
2025-03-13 18:34:28 +0100

    allow users to override unmarshaler (#29)

--- pkg/jetstream/subscriber.go
+++ pkg/jetstream/subscriber.go
@@ -7,12 +7,10 @@ import (
 	"time"
 
 	"github.com/ThreeDotsLabs/watermill"
-	wmnats "github.com/ThreeDotsLabs/watermill-nats/v2/pkg/nats"
 	"github.com/ThreeDotsLabs/watermill/message"
 	watermillSync "github.com/ThreeDotsLabs/watermill/pubsub/sync"
 	"github.com/nats-io/nats.go"
 	"github.com/nats-io/nats.go/jetstream"
-	"github.com/pkg/errors"
 )
 
 var _ message.Subscriber = &Subscriber{}
@@ -34,6 +32,7 @@ type Subscriber struct {
 	configureConsumer ConsumerConfigurator
 	consumeOptions    []jetstream.PullConsumeOpt
 	ackAsync          bool
+	unmarshaler       Unmarshaler
 }
 
 // NewSubscriber creates a new watermill JetStream subscriber.
@@ -46,7 +45,6 @@ func NewSubscriber(config SubscriberConfig) (*Subscriber, error) {
 	if nc == nil {
 		var err error
 		nc, err = nats.Connect(config.URL)
-
 		if err != nil {
 			return nil, fmt.Errorf("failed to connect: %w", err)
 		}
@@ -75,6 +73,7 @@ func newSubscriber(nc *nats.Conn, config *SubscriberConfig) (*Subscriber, error)
 		consumeOptions:    config.ConsumeOptions,
 		ackAsync:          config.AckAsync,
 		nakDelay:          config.NakDelay,
+		unmarshaler:       config.Unmarshaler,
 	}, nil
 }
 
@@ -132,34 +131,4 @@ func (s *Subscriber) Close() error {
 	}
 
 	return nil
-}
-
-// unmarshal turns a given JetStream message into a watermill message.
-// Reserved NATS headers are not propagated in the resulting message Metadata.
-func unmarshal(msg jetstream.Msg) (*message.Message, error) {
-	data := msg.Data()
-
-	hdr := msg.Headers()
-
-	id := hdr.Get(wmnats.WatermillUUIDHdr)
-
-	md := make(message.Metadata)
-
-	for k, v := range hdr {
-		switch k {
-		case wmnats.WatermillUUIDHdr, nats.MsgIdHdr, nats.ExpectedLastMsgIdHdr, nats.ExpectedStreamHdr, nats.ExpectedLastSubjSeqHdr, nats.ExpectedLastSeqHdr:
-			continue
-		default:
-			if len(v) == 1 {
-				md.Set(k, v[0])
-			} else {
-				return nil, errors.Errorf("multiple values received in NATS header for %q: (%+v)", k, v)
-			}
-		}
-	}
-
-	wm := message.NewMessage(id, data)
-	wm.Metadata = md
-
-	return wm, nil
 }
