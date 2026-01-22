56723dac46753d3e9498f6081e90b19a49ce62ad
Pavel Kiselev <paxa131997@gmail.com>
2026-01-22 13:03:39 +0100

    Exported config field now follows naming conventions
    Co-authored-by: Miłosz Smółka <milosz.smolka@gmail.com>

--- pkg/jetstream/config.go
+++ pkg/jetstream/config.go
@@ -21,8 +21,8 @@ type PublisherConfig struct {
 	// ConfigureStream is a custom function that can be used to define stream configuration from a topic.  Publisher uses it to calculate publish destination from topic.
 	ConfigureStream StreamConfigurator
 
-	// TrackMsgId sets Nats-Msg-Id to the msg UUID to prevent duplication (needed for exactly once processing).
-	TrackMsgId bool
+	// TrackMessageID sets Nats-Msg-Id to the msg UUID to prevent duplication (needed for exactly once processing).
+	TrackMessageID bool
 }
 
 // setDefaults sets default values needed for a publisher if unset
